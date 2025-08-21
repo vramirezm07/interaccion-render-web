0a87bb36b7ed9424690956815b38ca8508f5bea8
kuaki <vramirezm@centro.edu.mx>
2025-08-21 13:10:29 -0600

    Ejercicio 02

--- Sesion-03/ejercicio-02/app.js
+++ Sesion-03/ejercicio-02/app.js
@@ -15,7 +15,7 @@ canvas.height=window.innerHeight;
 
 // 4. instrucciones para dibujar un circulo 
 ctx.beginPath();
-ctx.strokeStyle = "black"; // Color del borde
+ctx.strokeStyle = "#8C3B4A"; // Color del borde
 ctx.lineWidth = 10; // Ancho del borde
 ctx.ellipse(100, 100, 70, 70, 0, 0, Math.PI*2); // Dibuja un círculo en el centro
 ctx.stroke(); // Aplica el trazo
@@ -26,7 +26,7 @@ ctx.closePath(); // Cierra el camino actual
 //con parámmetros para dibuar cirulos en difenretes posiciones sin repetir código
 function dibujarCirculo(x, y) {
     ctx.beginPath();
-    ctx.strokeStyle = "black"; // Color del borde
+    ctx.strokeStyle = "#8C3B4A"; // Color del borde
     ctx.lineWidth = 10; // Ancho del borde
     ctx.ellipse(x, y, 70,70, 0, 0, Math.PI*2); 
     ctx.stroke(); // Aplica el trazo
@@ -36,9 +36,27 @@ function dibujarCirculo(x, y) {
 dibujarCirculo(200, 100); // Dibuja un círculo en la posición (200, 200)
 dibujarCirculo(300, 150); // Dibuja un círculo en la posición (300, 200)
 
-//6. Escuchar el evento de movemouse para dibujar circulos
-window.addEventListener("mousemove", function(Loaiza) {
-    
-    // Dibujar un círculo en la posición del mouse
-    dibujarCirculo(Loaiza.x, Loaiza.y);
+const circulo = {
+    colorLinea: "#8C3B4A",
+    grosorLinea: 10, 
+    radio: 100,
+    rotacion: 0,
+    anguloInicio: 0,
+    anguloFinal: Math.PI*2,
+    x:0,
+    y:0,
+    dibujar:function(x,y) {
+        ctx.beginPath();
+        ctx.strokeStyle = circulo.colorLinea; // Color del borde
+        ctx.lineWidth = circulo.grosorLinea; // Ancho del borde
+        ctx.ellipse(x, y, circulo.radio,circulo.radio, circulo.rotacion, circulo.anguloInicio, circulo.anguloFinal); 
+        ctx.stroke(); // Aplica el trazo
+    }
+}
+
+circulo.dibujar(400,400);
+
+// Crear event listener 
+window.addEventListener("mousedown", function(eventData) {
+    circulo.dibujar(eventData.x, eventData.y);
 });
